ad305242e1a761c6a3ff8bed293384aa942455f2
이홍렬 <ryullee0911@gmail.com>
2024-06-26 22:50:11 +0900

    Initial Commit

---
+++ HC.OCHOICE.TV/HC_OCHOICE_TVApp.swift
@@ -0,0 +1,17 @@
+//
+//  HC_OCHOICE_TVApp.swift
+//  HC.OCHOICE.TV
+//
+//  Created by homechoic on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct HC_OCHOICE_TVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
